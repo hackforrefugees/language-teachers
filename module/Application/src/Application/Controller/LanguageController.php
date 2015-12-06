d4fcbbc4f9825ecffa69dbb344548f4cb53a5a19
Dominik Einkemmer <dominik_einkemmer@gmx.at>
2015-12-06 12:52:56 +0100

    Test for authentication over multiple angularjs pages

--- module/Application/src/Application/Controller/LanguageController.php
+++ module/Application/src/Application/Controller/LanguageController.php
@@ -2,7 +2,7 @@
 
 namespace Application\Controller;
 
-
+use Zend\Authentication\AuthenticationService;
 use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\JsonModel;
 
@@ -11,13 +11,23 @@ use Zend\View\Model\JsonModel;
  * @package Application\Controller
  * @author Dominik Einkemmer
  */
-class LanguageController extends AbstractRestfulController{
+class LanguageController extends AbstractRestfulController
+{
 
-    public function indexAction(){
+    public function indexAction()
+    {
+        $authService = new AuthenticationService();
+        if ($authService->hasIdentity()) {
+            $session = $authService->getStorage()->read();
+            $userId = $session['userGroup'];
+            die(var_dump($userId));
+        } else {
+            die(var_dump("Not authenticated"));
+        }
         $objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
         $languages = $objectManager->getRepository('Application\Entity\LtLanguage')->findAll();
         $languageList = array();
-        foreach($languages as $language){
+        foreach ($languages as $language) {
             $languageList[$language->getLangcode()] = $language->getLanguagename();
         }
         return new JsonModel($languageList);
